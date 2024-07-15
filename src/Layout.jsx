f3457da918cada37da1550d6f7bb15ab4dba4ff4
NAVIN CHAUDHARY <154901815+Navin-Chaudhary@users.noreply.github.com>
2024-07-15 21:38:07 +0530

    :heart: build fix

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header/Header.jsx'
+import Header from './components/header/Header'
 import Footer from './components/footer/footer'
 import { Outlet } from 'react-router-dom'
 
