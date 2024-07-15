c8c1108b3928f222801bc4302a39cb745755e08a
NAVIN CHAUDHARY <154901815+Navin-Chaudhary@users.noreply.github.com>
2024-07-15 21:41:18 +0530

    :heart: build fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import "./Header.js";
-import themechanger from "./Header.js";
 export default function Header() {
   return (
     <>
